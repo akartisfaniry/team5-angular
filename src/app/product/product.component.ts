efcabb8be31504eb4dcb7f9c6309090a1189d104
Riana9711 <riana.back@gmail.com>
2022-03-23 11:50:16 +0300

    feat: pagination_list_produit | 2. initialisation variable utiles

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiService} from '../service/api.service';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
     selector: 'app-product',
@@ -18,22 +19,32 @@ export class ProductComponent implements OnInit {
     ) {
     }
 
-    ngOnInit(): void {
-        this.getProducts({name: this.productName, categ: "",  offset: 0, limit: 20});
-    }
-
     private getProducts(request: any){
         this.apiService.getSearchAdvancedProducts(request).subscribe(
             (data: {}) => {
                 //convert list to array if not yet
-                data = Array.isArray(data) ? data : [data]
+                let resp = Array.isArray(data) ? data : [data]
                 console.log("data")
-                console.log({data})
-                this.products = data;
+                console.log({resp})
+                this.products = resp;
+                this.totalElements = resp.length;
             }, (err) => {
                 this.products = [];
                 this.totalElements = 0;
             });
+    }
+
+    ngOnInit(): void {
+        this.getProducts({name: this.productName, categ: "",  offset: 0, limit: 10});
+    }
+
+    nextPage(event: PageEvent) {
+        const request: any = {};
+        request['name'] = this.productName;
+        request['categ'] = "";
+        request['offset'] = event.pageIndex.toString();
+        request['limit'] = event.pageSize.toString();
+        this.getProducts(request);
     }
     _search(event: any): void {
 
