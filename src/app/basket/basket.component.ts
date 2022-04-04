292adc9573300461f8472575006c073f7884aa58
Sitraka Faniriandrandraina <sitraka@adrware.mg>
2022-04-04 22:18:45 +0300

    managa action in basket

--- src/app/basket/basket.component.ts
+++ src/app/basket/basket.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 import { ApiService } from '../service/api.service';
 
 @Component({
@@ -18,6 +19,50 @@ export class BasketComponent implements OnInit {
       .subscribe(data => {
         this.baskets = data
       });
+  }
+
+  _deleteCommande(id:number){
+    const currentComponent = this;
+    Swal.fire({
+      title: 'Êtes - vous sure?',
+      text: 'Cette commande sera supprimée.',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Oui',
+      cancelButtonText: 'Non',
+    }).then(async function(result) {
+      if (result.value) {
+        let responseData = await currentComponent.apiService.removeBasket(id);
+        if (responseData!=null && responseData.status === 'success'){
+          Swal.fire('Panier','La commande a été supprimée', 'success');
+          currentComponent.ngOnInit();
+        }
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        console.log('Annuler');
+      }
+    });
+  }
+
+  _doCommande(id:number) {
+    const currentComponent = this;
+    Swal.fire({
+      title: 'Voulez - vous vraiment commander ce produit ?',
+      text: 'Nous serons informé de votre commande.',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonText: 'Oui',
+      cancelButtonText: 'Non',
+    }).then(async function(result) {
+      if (result.value) {
+        let responseData = await currentComponent.apiService.validateBasket(id);
+        if (responseData!=null && responseData.status === 'success'){
+          Swal.fire('Panier','Demande envoyée avec succès', 'success');
+          currentComponent.ngOnInit();
+        }
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        console.log('Annuler');
+      }
+    });
   }
 
 }
